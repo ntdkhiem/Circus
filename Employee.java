e4871849a0b50aeb2fcc2487b422e8647f2a7c3e
TopKeingt <ppkhiemnguyen@gmail.com>
2019-12-22 18:28:19 -0500

    created Employee class

---
+++ Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+    public String firstName, lastName, middleInitial, ss, title, category;
+
+    public Employee(String f, String l, String m, String s, String t, String c) {
+        firstName = f;
+        lastName = l;
+        middleInitial = m;
+        ss = s;
+        title = t;
+        category = c;
+    }
+
+}
